a07141dbbb15f24d6a86243f3762aea76aa67d92
Shenoy <chethan.kumar@everi.com>
2022-09-04 10:01:53 +0530

    Prod CRUD

---
+++ src/app/prod-add-kom/prod-add-kom.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PrdSvc } from '../prd-svc.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'prod-add-kom',
+  templateUrl: './prod-add-kom.component.html',
+  styleUrls: ['./prod-add-kom.component.scss'],
+})
+export class ProdAddKomComponent implements OnInit {
+  @Input() PopNgModal: any;
+
+  constructor(public PrdSvcVar: PrdSvc, public PopMdlVap: NgbModal) {}
+
+  AddProdVar = {
+    TtlVak: '',
+    DtlVak: '',
+    CstVak: 0,
+    ImgVak: 'assets/imgFdr/benq.jpg',
+  };
+
+  AddProdFnc() {
+    this.PrdSvcVar.PrdAryVar.push(this.AddProdVar);
+    this.PopNgModal.close();
+  }
+
+  ngOnInit(): void {}
+}
